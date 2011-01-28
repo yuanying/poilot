80df2b15b7345cfb49075ce4ab1fadbefee7d386
yuanying <yuanying@fraction.jp>
2011-01-28 14:26:23 +0900

    複数行コメントできるように修正。

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -9,26 +9,41 @@ socket.on('message', function(message) {
     $('#count').text(message.count);
   }
   if (message.message) {
-    var data = message.message;
-    var date = new Date();
-    date.setTime(data.time);
-    var div = $('<p class="chatlog"></p>');
-    div.text(data.text);
+    var data = message.message.text;
+    var div = null;
+    if (data.match(/\n/m)) {
+      div = $('<pre class="chatlog"></pre>');
+    } else {
+      div = $('<p class="chatlog"></p>');
+    }
+    div.text(data);
     $('#chat').prepend(div);
     $('#chat').scrollTop(1000000);
   }
 });
 
 var send = function(event) {
-  var text = $('#text').val();
+  var text = $(this.text).val();
   if (text) {
     var time = new Date().getTime();
     socket.send(json({message: {text: text, time: time}}));
-    $('#text').val('');
+    $(this.text).val('');
   }
   return false;
 }
 
 $('.sendForm').bind('submit', send);
+$('a[href="#textForm"]').bind('click', function() {
+  $('a[href="#textForm"]').closest('li').removeClass('disabled');
+  $('a[href="#textAreaForm"]').closest('li').addClass('disabled');
+  $('#textForm').removeClass('disabled');
+  $('#textAreaForm').addClass('disabled');
+});
+$('a[href="#textAreaForm"]').bind('click', function() {
+  $('a[href="#textAreaForm"]').closest('li').removeClass('disabled');
+  $('a[href="#textForm"]').closest('li').addClass('disabled');
+  $('#textAreaForm').removeClass('disabled');
+  $('#textForm').addClass('disabled');
+});
 
 });
